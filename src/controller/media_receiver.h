799daaa7ab8468cc746ca80473823ff12544f106
Zhennan Tu <zhennan.tu@gmail.com>
2021-03-04 01:39:58 +0800

    controller 2

--- src/controller/media_receiver.h
+++ src/controller/media_receiver.h
@@ -9,9 +9,15 @@ class MediaReceiver {
 public:
     MediaReceiver();
     void start();
+    void set_executor(); // or config in ctor
     void set_transport(std::unique_ptr<Transport>&& transport);
     void set_decoder(std::unique_ptr<VideoDecoderInterface>&& decoder);
     void set_render(std::unique_ptr<RenderInterface>&& render);
+
+    void on_packet();
+    void on_frame();
+    void on_decodable_frame();
+    void on_decoded_frame();
 
 private:
     std::unique_ptr<Transport> transport_;
--- src/controller/media_sender.h
+++ src/controller/media_sender.h
@@ -10,9 +10,15 @@ class MediaSender {
 public:
     MediaSender();
     void start();
+    void set_executor(); //or config in ctor
     void set_transport(std::unique_ptr<Transport>&& transport);
     void set_encoder(std::unique_ptr<VideoEncoderInterface>&& encoder);
     void set_capture(std::unique_ptr<VideoCaptureInterface>&& capture);
+
+    void on_raw_frame();
+    void on_encoded_frame();
+    void on_packet();
+    void on_sendable_packet();
 
 private:
     std::unique_ptr<Transport> transport_;
